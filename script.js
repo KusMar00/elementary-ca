59de113c59852a2373838b4034cd2779084d8336
Markus Johansen <markusraunjohansen@gmail.com>
2024-01-12 17:03:55 +0100

    bug fix

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
-const size = 10;
+const size = 5;
+const width = 800;
+const height = 700;
 
-let row = new Array(710 / 10);
-let ruleset = [0, 1, 0, 1, 0, 1, 1, 0];
+let rulevalue = 54;
+let row = new Array(width / size).fill(0);
 
 const initRow = () => {
   for (let i = 0; i < row.length; i++) {
-    row[i] = Math.random() < 0.5 ? 0 : 1;
+    row[Math.floor(row.length / 2)] = 1;
   }
 };
 
 const draw = () => {
   var c = document.getElementById("canvas");
+  c.width = width;
+  c.height = height;
   var ctx = c.getContext("2d");
+  initRow();
 
-  for (let j = 0; j < 500 / 10; j++) {
-    row = nextGeneration();
-    let x = 0;
+  for (let j = 0; j < height / size; j++) {
     let y = j * size;
-    ctx.moveTo(x, y);
+    ctx.moveTo(0, y);
     for (let i = 0; i < row.length; i++) {
       if (row[i] === 1) {
-        ctx.fillRect(x, y, size, size);
+        ctx.fillRect(size * i, y, size, size);
       }
-      x += size;
     }
+    row = nextGeneration();
   }
 };
 
-const calculateState = (left, right, state) => {
-  let neighborhood = "" + left + right + state;
+const calculateState = (a, b, c) => {
+  let ruleset = rulevalue.toString(2);
+  while (ruleset.length < 8) {
+    ruleset = "0" + ruleset;
+  }
+  let neighborhood = "" + a + b + c;
   let value = 7 - parseInt(neighborhood, 2);
-  return ruleset[value];
+  return parseInt(ruleset[value]);
 };
 
 const nextGeneration = () => {
   let nextRow = [];
-  for (let i = 0; i < row.length; i++) {
-    if (i === 0 || i === row.length - 1) {
-      nextRow[i] = row[i];
-      continue;
-    }
-    let left = row[i - 1];
-    let right = row[i + 1];
+  let len = row.length;
+  for (let i = 0; i < len; i++) {
+    let left = row[(i - 1 + len) % len];
+    let right = row[(i + 1) % len];
     let state = row[i];
-    let nextState = calculateState(left, right, state);
+    let nextState = calculateState(left, state, right);
     nextRow[i] = nextState;
   }
   return nextRow;
 };
-
-initRow();
 
 draw();
